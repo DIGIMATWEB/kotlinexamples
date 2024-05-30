3879ea60c8907abc50c6b2044e6e18574c99fe37
DIGIMAT <36770624+DIGIMATWEB@users.noreply.github.com>
2024-05-29 20:11:11 -0600

    update main

--- testApp/app/src/main/java/com/newlandapps/testApp/Programador.kt
+++ testApp/app/src/main/java/com/newlandapps/testApp/Programador.kt
@@ -1,7 +1,11 @@
 package com.newlandapps.testApp
 
-class Programador(val name:String, val age:Int, val lenguages:Array<String>) {
-
+class Programador(val name:String, val age:Int, val lenguages:Array<String>,val AutosFavoritos:Array<Carros>?=null,val friends:Array<Programador>? = null) {
+    enum class Carros{
+        MERCEDEZ,
+        MAZDA,
+        LOTUS
+    }
     fun code(){
         for (lenguage: String in lenguages){
             println("Se programar en $lenguage")
